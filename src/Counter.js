071fabd8107f2caa68523add7ecfcd42791b2bcd
joelcr10 <joelcraju@gmail.com>
2023-03-17 19:08:36 +0530

    added footer

--- src/Counter.js
+++ src/Counter.js
@@ -3,25 +3,25 @@ const Counter = () => {
         <div class="wrapper parallax" id="counter-container">
             <div class="counter col_fourth">
             
-            <h2 class="timer count-title count-number" data-to="700" data-speed="1500"></h2>
+            <h2 class="timer count-title count-number" data-to="700" data-speed="1500">700</h2>
             <p class="count-text ">ACM Members</p>
             </div>
 
             <div class="counter col_fourth">
-            <i class="fa fa-coffee fa-2x"></i>
-            <h2 class="timer count-title count-number" data-to="28" data-speed="1500"></h2>
+            {/* <i class="fa fa-coffee fa-2x"></i> */}
+            <h2 class="timer count-title count-number" data-to="28" data-speed="1500">28</h2>
             <p class="count-text ">Execom Members</p>
             </div>
 
             <div class="counter col_fourth">
-            <i class="fa fa-lightbulb-o fa-2x"></i>
-            <h2 class="timer count-title count-number" data-to="20" data-speed="1500"></h2>
+            {/* <i class="fa fa-lightbulb-o fa-2x"></i> */}
+            <h2 class="timer count-title count-number" data-to="20" data-speed="1500">20</h2>
             <p class="count-text ">Workshop Hosted</p>
             </div>
 
             <div class="counter col_fourth end">
-            <i class="fa fa-bug fa-2x"></i>
-            <h2 class="timer count-title count-number" data-to="157" data-speed="1500"></h2>
+            {/* <i class="fa fa-bug fa-2x"></i> */}
+            <h2 class="timer count-title count-number" data-to="157" data-speed="1500">157</h2>
             <p class="count-text ">Project Completed</p>
             </div>
         </div>
